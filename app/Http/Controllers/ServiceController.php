47828f43b9d6a2d1aeed38291876b0d2ed3709e7
AbelMiraval <abelmiraval@gmail.com>
2018-07-13 20:32:22 -0500

    Modify Service Controller

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -9,8 +9,12 @@ class ServiceController extends Controller
 {
     //
    public function listUsersCoursesOnline(){
-        $suscriptions = Suscription::where('system_id','1')->get();
+        $suscriptions = Suscription::join('users','suscriptions.user_id','=','users.id')
+        				->select('suscriptions.enddate','users.email')
+        				->where('suscriptions.system_id','=','1')
+        				->get();
         return response()->json($suscriptions);
+  		
    }
 
 }
